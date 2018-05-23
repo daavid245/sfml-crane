15e14af7f024429b721645c03c202a6d3ceda309
Dawid Kawałko <23136935+daavid245@users.noreply.github.com>
2018-05-23 10:26:50 +0200

    Dostosowano rozmiary okna

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char** argv)
 {
 	unsigned width = 1280;
-	unsigned height = 720;
+	unsigned height = 900;
 	std::string title = "Wizualizacja systemow automatyki - dzwig";
 	
 	CCrane simulation(width, height, title);

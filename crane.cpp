acfffeafff68f074ca11e8e1a08bc2cdbc38089f
Dawid Kawałko <23136935+daavid245@users.noreply.github.com>
2018-05-23 22:04:04 +0200

    Dodano podświetlanie strzałek po naciśnięciu

--- crane.cpp
+++ crane.cpp
@@ -1,12 +1,16 @@
 #include "crane.hpp"
 
 CCrane::CCrane(const unsigned width, const unsigned height, const std::string title)
-: IMGPATH_CRANE("assets/crane.png"), IMGPATH_HOOKCONTROL("assets/hookcontrol.png"), IMGPATH_HOOK("assets/hook.png")
+: IMGPATH_CRANE("assets/crane.png"), IMGPATH_HOOKCONTROL("assets/hookcontrol.png"), IMGPATH_HOOK("assets/hook.png"), 
+  IMGPATH_ARROWS("assets/arrows.png")
 {
 	m_width = width;
 	m_height = height;
 	
-	m_window.create(sf::VideoMode(m_width, m_height), title, sf::Style::Close);
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+	
+	m_window.create(sf::VideoMode(m_width, m_height), title, sf::Style::Close, settings);
 	m_window.setFramerateLimit(120);
 	
 	if (!loadSprites())
@@ -14,6 +18,8 @@ CCrane::CCrane(const unsigned width, const unsigned height, const std::string ti
 		std::cout << "Nie mozna wczytac obrazow\n";
 		m_window.close();
 	}
+	
+	m_hookRope.setFillColor(sf::Color::Black);
 }
 
 void CCrane::run()
@@ -43,35 +49,70 @@ void CCrane::processEvents()
 void CCrane::refresh()
 {
 	m_window.clear(sf::Color(255, 255, 255));
+	
+	for (int i = 0; i < m_arrowSprites.size(); i++)
+	{
+		m_window.draw(m_arrowSprites[i]);
+	}	
+	
 	m_window.draw(m_craneSprite);
 	m_window.draw(m_hookControlSprite);
 	m_window.draw(m_hookSprite);
 	m_window.draw(m_hookRope);
+	
 	m_window.display();
 }
 
 void CCrane::update()
 {
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && m_hookControlSprite.getPosition().x > m_craneSprite.getPosition().x + 10)
+	for (int i = 0; i < m_arrowSprites.size(); i++)
 	{
-		m_hookControlSprite.move(-1, 0);
-		m_hookSprite.move(-1, 0);
+		if (m_arrowSprites[i].getColor().a == 100)
+		{
+			m_arrowSprites[i].setColor(sf::Color(255, 255, 255, 255));
+		}
+	}
+		
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		if (m_hookControlSprite.getPosition().x > m_craneSprite.getPosition().x + 10)
+		{
+			m_hookControlSprite.move(-1, 0);
+			m_hookSprite.move(-1, 0);
+		}
+		
+		m_arrowSprites[Direction::LEFT].setColor(sf::Color(255, 255, 255, 100));		
 	}
 	
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && m_hookControlSprite.getPosition().x < m_craneSprite.getPosition().x + 350)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 	{
-		m_hookControlSprite.move(1, 0);
-		m_hookSprite.move(1, 0);
+		if (m_hookControlSprite.getPosition().x < m_craneSprite.getPosition().x + 350)
+		{
+			m_hookControlSprite.move(1, 0);
+			m_hookSprite.move(1, 0);
+		}
+		
+		m_arrowSprites[Direction::RIGHT].setColor(sf::Color(255, 255, 255, 100));
 	}
 	
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && m_hookSprite.getPosition().y > m_hookControlSprite.getPosition().y + m_hookControlTexture.getSize().y)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 	{
-		m_hookSprite.move(0, -1);
+		if (m_hookSprite.getPosition().y > m_hookControlSprite.getPosition().y + m_hookControlTexture.getSize().y)
+		{
+			m_hookSprite.move(0, -1);
+		}
+		
+		m_arrowSprites[Direction::UP].setColor(sf::Color(255, 255, 255, 100));
 	}
 	
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && m_hookSprite.getPosition().y < m_height - 60)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
 	{
-		m_hookSprite.move(0, 1);
+		if (m_hookSprite.getPosition().y < m_height - 60)
+		{
+			m_hookSprite.move(0, 1);
+		}
+		
+		m_arrowSprites[Direction::DOWN].setColor(sf::Color(255, 255, 255, 100));
 	}
 	
 	m_hookRope.setPosition(sf::Vector2f(m_hookSprite.getPosition().x + 3, m_hookControlSprite.getPosition().y + m_hookControlTexture.getSize().y));
@@ -80,7 +121,8 @@ void CCrane::update()
 
 bool CCrane::loadSprites()
 {
-	if (!m_craneTexture.loadFromFile(IMGPATH_CRANE) || !m_hookControlTexture.loadFromFile(IMGPATH_HOOKCONTROL) || !m_hookTexture.loadFromFile(IMGPATH_HOOK))
+	if (!m_craneTexture.loadFromFile(IMGPATH_CRANE) || !m_hookControlTexture.loadFromFile(IMGPATH_HOOKCONTROL) || !m_hookTexture.loadFromFile(IMGPATH_HOOK)
+		|| !m_arrowsTexture.loadFromFile(IMGPATH_ARROWS))
 	{
 		return false;
 	}
@@ -94,7 +136,18 @@ bool CCrane::loadSprites()
 	m_hookSprite.setPosition(m_hookControlSprite.getPosition().x + 10, m_craneSprite.getPosition().y + 177);
 	m_hookSprite.setTexture(m_hookTexture);
 	
-	m_hookRope.setFillColor(sf::Color::Black);
+	for (int i = 0; i < 4; i++)
+	{
+		sf::Sprite sprite;
+		sprite.setTexture(m_arrowsTexture);
+		sprite.setTextureRect(sf::IntRect(i*62, 0, 62, 62));
+		m_arrowSprites.push_back(sprite);
+	}
+	
+	m_arrowSprites[Direction::LEFT].setPosition(10, m_height - 72);
+	m_arrowSprites[Direction::UP].setPosition(77, m_height - 139);
+	m_arrowSprites[Direction::RIGHT].setPosition(144, m_height - 72);
+	m_arrowSprites[Direction::DOWN].setPosition(77, m_height - 72);
 	
 	return true;
 }
--- crane.hpp
+++ crane.hpp
@@ -4,6 +4,15 @@
 #define SFML_STATIC
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
+
+enum Direction
+{
+	LEFT,
+	UP,
+	RIGHT,
+	DOWN	
+};
 
 class CCrane
 {
@@ -17,14 +26,23 @@ class CCrane
 		const std::string IMGPATH_CRANE;
 		const std::string IMGPATH_HOOKCONTROL;
 		const std::string IMGPATH_HOOK;
+		const std::string IMGPATH_ARROWS;
 				
 		sf::RenderWindow m_window;
+		
+		// textures
 		sf::Texture m_craneTexture;
 		sf::Texture m_hookControlTexture;
 		sf::Texture m_hookTexture;
+		sf::Texture m_arrowsTexture;
+		
+		// sprites 
 		sf::Sprite m_craneSprite;
 		sf::Sprite m_hookControlSprite;
 		sf::Sprite m_hookSprite;
+		std::vector<sf::Sprite> m_arrowSprites;
+		
+		// other
 		sf::RectangleShape m_hookRope;
 		
 		void processEvents();

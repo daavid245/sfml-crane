9300434f0888fa4c52abc1b31802315939cb52a4
Dawid Kawałko <23136935+daavid245@users.noreply.github.com>
2018-05-24 08:36:20 +0200

    Dostosowano rozdzielczość

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char** argv)
 {
-	unsigned width = 1280;
-	unsigned height = 900;
+	unsigned width = 1400;
+	unsigned height = 870;
 	std::string title = "Wizualizacja systemow automatyki - dzwig";
 	
 	CCrane simulation(width, height, title);
